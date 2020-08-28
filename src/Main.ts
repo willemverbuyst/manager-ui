205b199f61231a778c936da5665ad4df6fad1613
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-28 21:13:49 +0200

    Set up webpack

---
+++ src/Main.ts
@@ -0,0 +1,14 @@
+import { Router } from './Router';
+
+export class Main {
+  private router: Router = new Router();
+
+  constructor() {
+    console.log('Constructed new Instance of the program');
+  }
+
+  public launchApp() {
+    this.router.handleRequest();
+  }
+}
+new Main().launchApp();
---
+++ src/Router.ts
@@ -0,0 +1,8 @@
+export class Router {
+  public handleRequest() {
+    console.log('Handling request for ' + this.getRoute());
+  }
+  private getRoute(): string {
+    return window.location.pathname;
+  }
+}
