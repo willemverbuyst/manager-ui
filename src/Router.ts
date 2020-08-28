79980e17e83a5c5b9f7e90bfe778e8ed8ae6384f
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-28 21:37:48 +0200

    Create main controller

--- src/Router.ts
+++ src/Router.ts
@@ -1,6 +1,19 @@
+import { MainController } from './controllers/MainController';
+
 export class Router {
+  private mainElement = document.getElementById('main-container');
+
   public handleRequest() {
     console.log('Handling request for ' + this.getRoute());
+
+    switch (this.getRoute()) {
+      default:
+        if (this.mainElement) {
+          const mainController: MainController = new MainController();
+          this.mainElement.append(mainController.createView());
+        }
+        break;
+    }
   }
   private getRoute(): string {
     return window.location.pathname;
---
+++ src/controllers/MainController.ts
@@ -0,0 +1,19 @@
+export class MainController {
+  public createView(): HTMLDivElement {
+    const container = document.createElement('div');
+
+    const title = document.createElement('h2');
+    title.innerText = 'Welcome to our main page!';
+
+    const article = document.createElement('div');
+    article.innerText =
+      ' Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.';
+
+    const button = document.createElement('button');
+    button.innerText = 'Login';
+
+    container.append(title, article, button);
+
+    return container;
+  }
+}
