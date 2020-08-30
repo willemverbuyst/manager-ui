26cfda4da656ab1a55fbef5a3e90054280883acb
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-30 09:06:38 +0200

    Access server with http cal

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,14 +1,27 @@
 import { SessionToken } from '../models/AuthenticationModels';
 
+const baseUrl = 'http://localhost:8080/';
+const loginUrl = baseUrl + 'login';
+
 export class LoginService {
   public async login(
     userName: string,
     password: string
   ): Promise<SessionToken | undefined> {
-    if (userName === 'user' && password === '123') {
-      return {
-        username: 'Some User',
-      } as any;
+    let options = {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: userName,
+        password: password,
+      }),
+    };
+
+    const result = await fetch(loginUrl, options);
+    if (result.status === 201) {
+      return await result.json();
     } else {
       return undefined;
     }
